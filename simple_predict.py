c8685a245ddf55d27af971a90abb09fb92074822
Ilya Gusev <phoenixilya@gmail.com>
2017-07-27 04:55:43 +0300

    0.465 spearman

--- simple_predict.py
+++ simple_predict.py
@@ -9,8 +9,10 @@ from nltk.stem.snowball import SnowballStemmer
 from nltk import pos_tag
 from sklearn.svm import LinearSVC
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import  Lasso
+from sklearn.linear_model import Lasso
 from scipy import stats
+from lightgbm import LGBMClassifier
+from sklearn.ensemble import BaggingClassifier
 
 from parse_data import parse_dir
 morph_ru = MorphAnalyzer()
@@ -21,13 +23,15 @@ def spearman(a, b):
     return stats.spearmanr(a, b)[0]
 
 
-def text_to_wordlist(sentence, cyrillic=False):
+def text_to_wordlist(sentence):
     regexp = "[^а-яА-Яёa-zA-Z.,;!?:()><_]"
-    if cyrillic:
-        regexp = "[^а-яА-Яё]"
     sentence = re.sub(regexp, " ", sentence)
     result = sentence.lower().split()
     return result
+
+
+def text_to_charlist(sentence):
+    return list(sentence)
 
 
 def stem_sentence(sentence, language):
@@ -93,8 +97,10 @@ def collect_all_features():
     df = parse_dir()
     data = pd.DataFrame()
     data["userMessages"] = df["AliceMessages"].tolist() + df["BobMessages"].tolist()
+    data["userOpponentMessages"] = df["BobMessages"].tolist() + df["AliceMessages"].tolist()
     data["userMessageMask"] = df["AliceMessageMask"].tolist() + df["BobMessageMask"].tolist()
     data["userConcatenatedMessages"] = data["userMessages"].apply(lambda x: " ".join(x))
+    data["userOpponentConcatenatedMessages"] = data["userMessages"].apply(lambda x: " ".join(x))
     data["userIsBot"] = df["AliceIsBot"].tolist() + df["BobIsBot"].tolist()
     data["userScores"] = df["AliceScore"].tolist() + df["BobScore"].tolist()
 
@@ -105,24 +111,46 @@ def collect_all_features():
     data["avgWords"] = data["userMessages"].apply(lambda x: np.mean([0] + [len(msg.split()) for msg in x]))
     data["msgInARow"] = data["userMessageMask"].apply(lambda x: count_in_a_row(x))
 
-    print("Bow step...")
+    # data["RmessageNum"] = data["userOpponentMessages"].apply(lambda x: len(x))
+    # data["RnumChars"] = data["userOpponentMessages"].apply(lambda x: sum([len(msg) for msg in x]))
+    # data["RnumWords"] = data["userOpponentMessages"].apply(lambda x: sum([len(msg.split()) for msg in x]))
+    # data["RavgChars"] = data["userOpponentMessages"].apply(lambda x: np.mean([0] + [len(msg) for msg in x]))
+    # data["RavgWords"] = data["userOpponentMessages"].apply(lambda x: np.mean([0] + [len(msg.split()) for msg in x]))
+
+    print("BoW step...")
     bow_train_data, _ = bow(data["userConcatenatedMessages"].tolist(), [], language='en', stem=True,
-                            tokenizer=text_to_wordlist, use_tfidf=False, bow_ngrams=(1, 2))
+                            tokenizer=text_to_wordlist, use_tfidf=False, bow_ngrams=(1, 1))
     data = pd.concat([data, pd.DataFrame(bow_train_data)], axis=1)
 
-    print("POS step...")
-    pos_train_data = []
-    for text in data["userConcatenatedMessages"].tolist():
-        pos_train_data.append(get_sentence_tags(text, "en"))
-    pos_train_data, _ = bow(pos_train_data, [], language='en', stem=False, tokenizer=text_to_wordlist,
-                            use_tfidf=False, bow_ngrams=(1, 1))
-    data = pd.concat([data, pd.DataFrame(pos_train_data)], axis=1)
+    print("RBoW step...")
+    bow_train_data, _ = bow(data["userOpponentConcatenatedMessages"].tolist(), [], language='en', stem=True,
+                            tokenizer=text_to_wordlist, use_tfidf=False, bow_ngrams=(1, 1))
+    data = pd.concat([data, pd.DataFrame(bow_train_data)], axis=1)
+
+    print("BoC step...")
+    bow_train_data, _ = bow(data["userConcatenatedMessages"].tolist(), [], language='en', stem=False,
+                            tokenizer=text_to_charlist, use_tfidf=False, bow_ngrams=(1, 1))
+    data = pd.concat([data, pd.DataFrame(bow_train_data)], axis=1)
+
+    print("RBoC step...")
+    bow_train_data, _ = bow(data["userOpponentConcatenatedMessages"].tolist(), [], language='en', stem=False,
+                            tokenizer=text_to_charlist, use_tfidf=False, bow_ngrams=(1, 1))
+    data = pd.concat([data, pd.DataFrame(bow_train_data)], axis=1)
+
+    # print("POS step...")
+    # pos_train_data = []
+    # for text in data["userConcatenatedMessages"].tolist():
+    #     pos_train_data.append(get_sentence_tags(text, "en"))
+    # pos_train_data, _ = bow(pos_train_data, [], language='en', stem=False, tokenizer=text_to_wordlist,
+    #                         use_tfidf=False, bow_ngrams=(1, 1))
+    # data = pd.concat([data, pd.DataFrame(pos_train_data)], axis=1)
     return data
 
 
 def answer_bot(data, train_indices, test_indices):
     answers = np.array([int(i) for i in data["userIsBot"].tolist()])
-    data = data.drop(["userMessages", "userMessageMask", "userConcatenatedMessages", "userIsBot", "userScores"], axis=1)
+    data = data.drop(["userMessages", "userMessageMask", "userConcatenatedMessages", "userIsBot",
+                      "userScores", "userOpponentMessages", "userOpponentConcatenatedMessages"], axis=1)
 
     l = data.shape[0]//2
     pairs = {i: l+i if i < l else i-l for i in range(2*l)}
@@ -141,7 +169,8 @@ def predict_regression(data, train_indices, test_indices):
     l = data.shape[0] // 2
     pairs = {i: l + i if i < l else i - l for i in range(2 * l)}
     bot_scores_indices = [pairs[i] for i in data[data['userIsBot'] == True].index.tolist()]
-    data = data.drop(["userMessages", "userMessageMask", "userConcatenatedMessages", "userIsBot", "userScores"], axis=1)
+    data = data.drop(["userMessages", "userMessageMask", "userConcatenatedMessages",
+                      "userIsBot", "userScores", "userOpponentMessages", "userOpponentConcatenatedMessages"], axis=1)
 
     train_indices += [pairs[i] for i in train_indices]
     test_indices += [pairs[i] for i in test_indices]
@@ -154,6 +183,7 @@ def predict_regression(data, train_indices, test_indices):
     for i, is_bot in enumerate(bot_answers):
         if is_bot == 1.0:
             preds[test_indices.index(pairs[test_indices[i]])] = 0
+    del data
     return spearman(answers[test_indices], preds)
 
 scores = []
@@ -162,6 +192,7 @@ for i in range(10):
     train_indices, test_indices, _, __ = train_test_split(range(features.shape[0]//2),
                                                           range(features.shape[0]//2), test_size=0.1, random_state=i)
     score = predict_regression(features, train_indices, test_indices)
+    print(score)
     scores.append(score)
 scores = np.array(scores)
 print("CV: %0.3f (+/- %0.3f)" % (scores.mean(), scores.std()))
